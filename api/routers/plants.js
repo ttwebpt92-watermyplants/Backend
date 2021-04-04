4672fb0e1a619e151bed1691e6128c777dd0366c
Amber Chunn <a.77@me.com>
2021-04-04 18:19:29 -0400

    Added Restrict Middleware to Plants Router

--- api/routers/plants.js
+++ api/routers/plants.js
@@ -1,29 +1,29 @@
 const express = require("express")
 const Plants = require('../models/plants');
 const router = require("express").Router();
-
+const { restricted } = require('../middleware/auth');
 
 // Add New Plant
-router.post("/",  (req, res, next) => {
+router.post("/",  restricted(), (req, res, next) => {
 	Plants.add(req.body)
 		.then((data) => res.status(201).json(data))
 		.catch((err) => next(err))
 })
 
 // Get All Plants
-router.get("/",  (req, res, next) => {
+router.get("/",  restricted(), (req, res, next) => {
 	Plants.find()
 		.then((data) => res.status(200).json(data))
 		.catch((err) => next(err))
 })
 
 // Get Specific Plant
-router.get("/:id", (req, res, next) => {
+router.get("/:id", restricted(), (req, res, next) => {
 	res.status(200).json(req.shout)
 })
 
 // Update Plant
-router.put('/:id',  async (req, res, next) => {
+router.put('/:id', restricted(), async (req, res, next) => {
     try {
         const data = await projects.update(req.params.id, req.body);
         res.status(200).json(data);
@@ -33,7 +33,7 @@ router.put('/:id',  async (req, res, next) => {
 })
 
 // Delete Plant
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", restricted(), (req, res, next) => {
 	Shouts.remove(req.params.id)
 		.then(() => res.status(204).end())
 		.catch((err) => next(err))
